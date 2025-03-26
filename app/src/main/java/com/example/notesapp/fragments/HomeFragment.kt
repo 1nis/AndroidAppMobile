2e8dbf7c38e1b95a128b2754fd3676334ed1abd8
1nis <anis@kherraf.fr>
2025-03-26 16:36:51 +0100

    Update HomeFragment.kt

--- app/src/main/java/com/example/notesapp/fragments/HomeFragment.kt
+++ app/src/main/java/com/example/notesapp/fragments/HomeFragment.kt
@@ -15,7 +15,7 @@ private const val ARG_PARAM2 = "param2"
 /**
  * A simple [Fragment] subclass.
  * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
+ * create an instance of this fragment
  */
 class HomeFragment : Fragment() {
     // TODO: Rename and change types of parameters
